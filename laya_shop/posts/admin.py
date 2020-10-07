c0d33ff442b634441b292dda0f4b90e12a948fe3
Leonel Ubeda <leonel@pop-os.localdomain>
2020-10-07 11:37:11 -0600

    SubCategory inline added to Category admin

--- laya_shop/posts/admin.py
+++ laya_shop/posts/admin.py
@@ -3,8 +3,13 @@ from .models import Category, Post, SubCategory
 # Register your models here.
 
 
+class SubCategoryInline(admin.StackedInline):
+    model = SubCategory
+    extra = 2
 
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (SubCategoryInline, )
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(SubCategory)
 admin.site.register(Post)
