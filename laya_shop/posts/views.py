60ed21f779b95d04f925af5c3e1e3b1dfd1cce8a
Leonel Ubeda <leonelubeda@hotmail.com>
2020-10-16 23:38:19 -0600

    ESTEEEEEEEEEm

--- laya_shop/posts/views.py
+++ laya_shop/posts/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView, TemplateView
 
 from django.core.cache import caches
-from .decorators import  cache_on_auth
+from .decorators import cache_on_auth
 
 from posts.filters import PostFilter
 from posts.mixins import PostClassificationMixin
 from posts.models import Post, Category, SubCategory
 
-
 cache = caches['default']
 
-@method_decorator(cache_on_auth(60*60), name="dispatch")
+
+@method_decorator(cache_on_auth(60 * 60), name="dispatch")
 class PostList(PostClassificationMixin, ListView):
     model = Post
     template_name = 'posts/search.html'
@@ -35,14 +35,14 @@ class PostList(PostClassificationMixin, ListView):
             print("NO CACHEADO!")
             queryset = self.model.objects.select_related('currency').prefetch_related('images')
             queryset = PostFilter(self.request.GET, queryset=queryset, request=self.request).qs
-            cache.set(cache_key, queryset, 60*60)
+            cache.set(cache_key, queryset, 60 * 60)
             return queryset
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['search_cache_name'] = self.build_cache_key()
         return context
+
 
 post_list_view = PostList.as_view()
 
@@ -53,11 +53,12 @@ class PostDetail(DetailView):
     template_name = 'posts/detail.html'
 
     def get_object(self):
-        queryset = Post.objects.select_related('business', 'currency').prefetch_related('locations', 'subcategories__category')
+        queryset = Post.objects.select_related('business', 'currency').prefetch_related('locations',
+                                                                                        'subcategories__category')
         return get_object_or_404(queryset, pk=self.kwargs['pk'])
 
-post_detail_view = PostDetail.as_view()
 
+post_detail_view = PostDetail.as_view()
 
 
 class CategoryDetail(DetailView):
@@ -76,7 +77,12 @@ class CategoryDetail(DetailView):
 
         context['posts'] = Post.objects.select_related('business').prefetch_related('images').all()[:6]
         context['subcategories'] = self.object.subcategories.all()
-        context['highlight'] = Post.objects.prefetch_related('images').filter(subcategories__category=self.object, highlighted=True).distinct()
+        context['highlight'] = Post.objects.prefetch_related('images').filter(subcategories__category=self.object,
+                                                                              highlighted=True).distinct()
+        context['subcategories_list'] = [
+            {'subcategory': subcategory, 'posts': Post.objects.prefetch_related('images').filter(subcategories=subcategory).exclude(id__in=context['highlight'])[:6] }
+            for subcategory in context['subcategories'][:3]]
         return context
+
 
 category_detail_view = CategoryDetail.as_view()
