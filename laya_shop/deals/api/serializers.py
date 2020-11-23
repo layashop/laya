29b42c7942d303aeac51cdbff6fc22a9f3cbcb07
Roberto Sanchez <sanchez.roberto0810@gmail.com>
2020-11-22 18:28:44 -0600

    Improve Deals API

--- laya_shop/deals/api/serializers.py
+++ laya_shop/deals/api/serializers.py
@@ -1,8 +1,34 @@
 from rest_framework import serializers
+from users.api.serializers import SimpleUserSerializer
+from business.models import Business
 from deals.models import Deal
 
 
 class DealSerializer(serializers.ModelSerializer):
     class Meta:
         model = Deal
-        fields = ['history', 'business', 'user', 'status', 'expires_at', 'created_at', 'id']
+        fields = [
+            "history",
+            "business",
+            "user",
+            "status",
+            "expires_at",
+            "created_at",
+            "id",
+        ]
+
+
+class DealUserSerializer(serializers.ModelSerializer):
+    user = SimpleUserSerializer()
+
+    class Meta:
+        model = Deal
+        fields = "__all__"
+
+
+class DealBusinessSerializer(serializers.ModelSerializer):
+    business = BusinessSerializer()
+
+    class Meta:
+        model = Deal
+        fields = "__all__"
--- laya_shop/deals/api/views.py
+++ laya_shop/deals/api/views.py
@@ -1,12 +1,38 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
+from django_filters import rest_framework as filters
+
 from deals.models import Deal
-from .serializers import DealSerializer
+from .serializers import DealSerializer, DealBusinessSerializer, DealUserSerializer
+from .filters import DealsFilters
 
 
 class DealViewSet(ModelViewSet):
     queryset = Deal.objects.all()
     serializer_class = DealSerializer
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
+    filter_backends = [filters.DjangoFilterBackend]
+    filter_class = DealsFilters
 
+    def get_queryset(self):
+        request_query_type = self.request.GET.get("type")
+        print("query type ", request_query_type)
+        related_name = {
+            "user": "business",
+            "business": "user",
+        }.get(request_query_type)
+
+        if related_name:
+            return Deal.objects.select_related(related_name).all()
+        return Deal.objects.select_related("user", "business").all()
+
+    def get_serializer_class(self):
+        request_query_type = self.request.GET.get("type")
+        serializer = {
+            "user": DealBusinessSerializer,
+            "business": DealUserSerializer,
+        }.get(request_query_type)
+        if serializer:
+            return serializer
+        return DealSerializer
