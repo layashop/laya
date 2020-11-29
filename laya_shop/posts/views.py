03b0667d51c5d26be8f8fc8d7b020563ba23aed9
Leonel Ubeda <leonelubeda@hotmail.com>
2020-11-28 22:25:21 -0600

    post routes y +

--- laya_shop/posts/views.py
+++ laya_shop/posts/views.py
@@ -55,7 +55,9 @@ class PostDetail(DetailView):
     def get_object(self):
         queryset = Post.objects.select_related('business', 'currency').prefetch_related('locations',
                                                                                         'subcategories__category')
-        return get_object_or_404(queryset, pk=self.kwargs['pk'], business__slug=self.kwargs['business_slug'])
+        slug: str = self.kwargs['business_slug']
+        slug = slug[1:] if slug.startswith('@') else slug
+        return get_object_or_404(queryset, pk=self.kwargs['pk'], business__slug=slug)
 
 
 post_detail_view = PostDetail.as_view()
