7ab0b5eedebe1bb85ccba104647f1b702b4660cb
Leonel Ubeda <leonelubeda@hotmail.com>
2020-12-10 00:32:21 -0600

    Featured home

--- laya_shop/internal/views.py
+++ laya_shop/internal/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
-from laya_shop.posts.models import Category
+from laya_shop.posts.models import Category, Post
 from laya_shop.internal.models import HomeSlide
-
+import random
 
 class HomeView(TemplateView):
     template_name = 'pages/home.html'
@@ -12,6 +12,8 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['slides'] = HomeSlide.objects.all()
+        # TODO: Componer esto por que en la documentacion dice que es ineficiente
+        context['featured'] = Post.objects.filter(highlighted=True).order_by('?')[:3]
         context['categories'] = Category.objects.all()[:15]
         return context
 
